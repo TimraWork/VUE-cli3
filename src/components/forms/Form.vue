1ed4d052714eddd6609233d2a6bf2a9b61912882
Elmira <timra.work@gmail.com>
2020-03-08 11:42:30 +0500

    ACF request

--- src/components/forms/Form.vue
+++ src/components/forms/Form.vue
@@ -5,20 +5,28 @@
 				h1.ui-title-1 FORM
 				form( name= "form")
 					.form__item
-						input(type="text" placeholder="Name" v-model="form.name" )
+						input(type="text" v-model="form.name")
 					.form__item
-						input(type="email" placeholder="Email" v-model="form.email" )
+						input(type="email" v-model="form.email")
 					.form__item
 						textarea( v-model="form.message" )
 					.buttons-list.button-list--info
-						button.button.button-default(type="submit") {{ $t('send') }}
+						button.button.button-default(type="submit" @click.prevent="onSubmit()") {{ $t('send') }}
 </template>
 <script>
+import axios from 'axios';
+// const sendUrl = 'http://localhost:8081/forms';
+// const sendUrl = 'https://timra.ru/timra/dist/mail.php';
+const sendUrl =
+	'https://timra.ru/timra/wp-json/contact-form-7/v1/contact-forms/8616/feedback';
+// const sendUrl = 'mail.php';
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 export default {
 	mounted() {
-		// this.form.name = 'Elmira';
-		// this.form.email = 'mer389@mail.ru';
-		// this.form.message = 'Test message';
+		this.form.name = 'Elmira';
+		this.form.email = 'mer389@mail.ru';
+		this.form.message = 'Test message';
 	},
 	data() {
 		return {
@@ -34,7 +42,30 @@ export default {
 	computed: {},
 	watch: {},
 	methods: {
-		onSubmit: function() {}
+		createDbData() {
+			const form = {
+				name: this.form.name,
+				email: this.form.email,
+				mess: this.form.message
+			};
+		},
+		onSubmit: function() {
+			console.log('onSubmit');
+			axios
+				.post(sendUrl, form)
+				.then(response => {
+					// this.posts = response.data;
+					console.log(response.data);
+				})
+				.then(response => {
+					console.log(response);
+					this.errors = [];
+				})
+				.catch(error => {
+					this.errors = error.response.data.message;
+					console.log(errors);
+				});
+		}
 	},
 	created() {}
 };
