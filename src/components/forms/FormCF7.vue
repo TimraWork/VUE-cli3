623ee875af28cb893e2852d343360dfc4c4202c3
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-03-23 12:13:35 +0500

    process.env.NODE_ENV for crossdomen captcha

--- src/components/forms/FormCF7.vue
+++ src/components/forms/FormCF7.vue
@@ -17,7 +17,12 @@
 					.form__item
 						textarea( v-model="form.message"  :placeholder="$t('message')")
 					.form__item
-						vue-recaptcha(sitekey="6LfbreEUAAAAAOcJgK_fqvQVHhYxv0-UP0mGY1Mr", :loadRecaptchaScript="true", @verify="recaptchaVerified", :placeholder="$t('mess')")
+						vue-recaptcha(
+							:sitekey="recaptchaKey", 
+							:loadRecaptchaScript="true", 
+							@verify="recaptchaVerified", 
+							:placeholder="$t('mess')"
+						)
 						.text-center.mb-2.sory(v-if="input_err.recaptcha") {{ $t('captcha') }}
 					.buttons-list.button-list--info
 						button.button.button-default(type="submit" @click.prevent="onSubmit()") {{ $t('send') }}
@@ -29,119 +34,119 @@
 
 </template>
 <script>
-import axios from 'axios';
-import VueRecaptcha from 'vue-recaptcha';
-import { required, email, minLength } from 'vuelidate/lib/validators';
+import axios from "axios";
+import VueRecaptcha from "vue-recaptcha";
+import { required, email, minLength } from "vuelidate/lib/validators";
 
 // const sendUrl = 'https://timra.ru/timra/dist/mail.php';
 const sendUrl =
-	'https://timra.ru/timra/wp-json/contact-form-7/v1/contact-forms/8616/feedback';
+  "https://timra.ru/timra/wp-json/contact-form-7/v1/contact-forms/8616/feedback";
 
 export default {
-	mounted() {
-		// this.form.ename = 'Elmira';
-		// this.form.email = 'mer389@mail.ru';
-		// this.form.message = 'Test message';
-		$v.email = '';
-		$v.email.ename = '';
-	},
-	data() {
-		return {
-			url: '',
-			form: {
-				ename: '',
-				email: '',
-				message: ''
-			},
-			recaptchaVerify: null,
-			status: '',
-			errors: '',
-			input_err: {
-				ename: '',
-				email: '',
-				recaptcha: '',
-				message: ''
-			}
-		};
-	},
-	validations: {
-		ename: {
-			required
-		},
-		email: {
-			required,
-			email
-		},
-		password: {
-			required,
-			minLength: minLength(6)
-		}
-	},
-	components: { VueRecaptcha },
-	computed: {},
-	watch: {
-		recaptchaVerify: 'recaptchaVerified'
-	},
-	methods: {
-		createDbData() {},
-		recaptchaVerified() {
-			this.recaptchaVerify = true;
-			if (this.recaptchaVerify) {
-				this.input_err.recaptcha = '';
-			}
-		},
-		onSubmit: function(recaptchaToken) {
-			const form = {
-				_wpcf7: '8616',
-				_wpcf7_version: '5.1.7',
-				_wpcf7_locale: 'ru_RU',
-				_wpcf7_unit_tag: 'wpcf7-f8616-o1',
-				_wpcf7_container_post: '0',
-				ename: this.form.ename,
-				email: this.form.email,
-				mess: this.form.message,
-				recaptchaToken: recaptchaToken
-			};
-			var config = {
-				headers: {
-					'Content-Type': 'multipart/form-data'
-				}
-			};
-			const formData = new FormData();
-			formData.append('ename', this.form.ename);
-			formData.append('email', this.form.email);
+  created() {
+    if (process.env.NODE_ENV === "production") {
+      this.recaptchaKey = "6LdqdqIUAAAAANREZhN9o9kCf2kI-cVGNEf3rJcj";
+    } else {
+      this.recaptchaKey = "6LfbreEUAAAAAOcJgK_fqvQVHhYxv0-UP0mGY1Mr";
+    }
+  },
+  mounted() {},
+  data() {
+    return {
+      url: "",
+      form: {
+        ename: "",
+        email: "",
+        message: ""
+      },
+      recaptchaVerify: null,
+      recaptchaKey: null,
+      status: "",
+      errors: "",
+      input_err: {
+        ename: "",
+        email: "",
+        recaptcha: "",
+        message: ""
+      }
+    };
+  },
+  validations: {
+    ename: {
+      required
+    },
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    }
+  },
+  components: { VueRecaptcha },
+  computed: {},
+  watch: {
+    recaptchaVerify: "recaptchaVerified"
+  },
+  methods: {
+    createDbData() {},
+    recaptchaVerified() {
+      this.recaptchaVerify = true;
+      if (this.recaptchaVerify) {
+        this.input_err.recaptcha = "";
+      }
+    },
+    onSubmit: function(recaptchaToken) {
+      const form = {
+        _wpcf7: "8616",
+        _wpcf7_version: "5.1.7",
+        _wpcf7_locale: "ru_RU",
+        _wpcf7_unit_tag: "wpcf7-f8616-o1",
+        _wpcf7_container_post: "0",
+        ename: this.form.ename,
+        email: this.form.email,
+        mess: this.form.message,
+        recaptchaToken: recaptchaToken
+      };
+      var config = {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      };
+      const formData = new FormData();
+      formData.append("ename", this.form.ename);
+      formData.append("email", this.form.email);
 
-			// this.$refs.recaptcha.execute();
+      // this.$refs.recaptcha.execute();
 
-			if (this.recaptchaVerify) {
-				this.input_err.recaptcha = '';
-				axios
-					.post(sendUrl, formData)
-					.then(response => {
-						this.errors = response.data.message;
-						this.status = response.data.status;
+      if (this.recaptchaVerify) {
+        this.input_err.recaptcha = "";
+        axios
+          .post(sendUrl, formData)
+          .then(response => {
+            this.errors = response.data.message;
+            this.status = response.data.status;
 
-						if (response.data.status === 'validation_failed') {
-							response.data.invalidFields.forEach(element => {
-								const errorsArray = element.into.split('.');
-								const input_name =
-									errorsArray[errorsArray.length - 1];
-								const input_mes = element.message;
-								this.input_err[input_name] = input_mes;
-							});
-						} else {
-							this.input_err = '';
-						}
-					})
-					.catch(error => {
-						// this.errors = error.response.data.message;
-						// console.log('response = ', response);
-					});
-			} else {
-				this.input_err.recaptcha = true;
-			}
-		}
-	},
-	created() {}
+            if (response.data.status === "validation_failed") {
+              response.data.invalidFields.forEach(element => {
+                const errorsArray = element.into.split(".");
+                const input_name = errorsArray[errorsArray.length - 1];
+                const input_mes = element.message;
+                this.input_err[input_name] = input_mes;
+              });
+            } else {
+              this.input_err = "";
+            }
+          })
+          .catch(error => {
+            // this.errors = error.response.data.message;
+            // console.log('response = ', response);
+          });
+      } else {
+        this.input_err.recaptcha = true;
+      }
+    }
+  }
 };
 </script>
